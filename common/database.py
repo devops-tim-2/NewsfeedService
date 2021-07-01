c258bbba3436450db3a3c57710d897f64a57513d
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-07-01 20:19:56 +0200

    Add docker compose file (#14)

---
+++ common/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from os import environ
+# from models.models import Model
+
+engine = create_engine(environ.get('SQLALCHEMY_DATABASE_URI'))
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+def init_db():
+    # Model.query = db_session.query_property()
+    # Model.metadata.create_all(bind=engine)
+    pass
+
