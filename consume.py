e04227fbc796aa23c188004e2cc1e2a3ea4629c2
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-07-01 23:32:48 +0200

    Eventual posts closes #5 (#16)
    * Add model classes and helper functions
    
    * Add consumer layer for eventual consistency of posts and users
    
    * Fix sonarcloud reported code smells closes #5

--- consume.py
+++ consume.py
@@ -1,4 +1,4 @@
-# from broker.consumer import AdminConsumer, UserConsumer
+from broker.consumer import PostConsumer, UserConsumer
 import pika
 from os import environ
 
@@ -6,11 +6,12 @@ params = pika.URLParameters(environ.get('RABBITMQ_URI'))
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
-# user_queue = UserConsumer(channel)
-# admin_queue = AdminConsumer(channel)
 
-# print(f'Started user_queue: {type(user_queue)}')
-# print(f'Started admin_queue: {type(admin_queue)}')
+user_queue = UserConsumer(channel)
+post_queue = PostConsumer(channel)
+
+print(f'Started user_queue: {type(user_queue)}')
+print(f'Started post_queue: {type(post_queue)}')
 
 channel.start_consuming()
 channel.close()
