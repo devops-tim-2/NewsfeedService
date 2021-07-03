79a98531515d6691bf7c09eda14fa1d38b22d221
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-07-04 00:07:45 +0200

    Add pagination (#20)

--- repository/newsfeed_repository.py
+++ repository/newsfeed_repository.py
@@ -2,6 +2,6 @@ from common.database import db_session
 from models.models import PostFeed
 
 def get_for_user(user_id):
-    return PostFeed.query.filter(PostFeed.u_id == user_id).all()
+    return PostFeed.query.filter(PostFeed.u_id == user_id).order_by(PostFeed.p_id.desc()).all()
 
 
