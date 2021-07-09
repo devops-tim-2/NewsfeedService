50dc0b399bb87eb85ee3a07dca55209a595a2d71
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-07-09 18:46:30 +0200

    Implement admin panel and campaign newsfeed support (#23)

--- repository/newsfeed_repository.py
+++ repository/newsfeed_repository.py
@@ -1,7 +1,9 @@
 from common.database import db_session
-from models.models import PostFeed
+from models.models import PostFeed, CampaignFeed
 
 def get_for_user(user_id):
     return PostFeed.query.filter(PostFeed.u_id == user_id).order_by(PostFeed.p_id.desc()).all()
 
 
+def get_campaigns_for_user(user_id):
+    return CampaignFeed.query.filter(CampaignFeed.u_id == user_id).order_by(CampaignFeed.c_id.desc()).all()
